116d64ee344b4998a52f21e94fbf0e6a22d2870f
Michal <michal.matuszewski1984@gmail.com>
2026-05-31 00:28:18 +0200

    Fix public CI bootstrap

--- src/config/certificates.h
+++ src/config/certificates.h
@@ -1,6 +1,10 @@
 #ifndef CERTIFICATES_H
 #define CERTIFICATES_H
 
+#if __has_include("certificates.local.h")
 #include "certificates.local.h"
+#else
+#include "certificates.local.example.h"
+#endif
 
 #endif
